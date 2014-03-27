697160fa6efc57c451abbf6901136504c04d2aec
can jin <cjin7@dons.usfca.edu>
2014-03-27 14:29:03 -0700

    update

--- homework1/README.md
+++ homework1/README.md
@@ -7,40 +7,60 @@ Homework 1: Basic Charts
 
 ## Instructions ##
 
-[INSTRUCTIONS: Include instructions on how to run your code. See [`homework0`](../homework0) for an example.]
-
 The following packages must be installed prior to running this code:
 
 - `ggplot2`
 - `devtools`
-- `plyr`
+
 
 To run this code, please enter the following commands in R:
 
 ```
 library(devtools)
-source_url("https://raw.github.com/sjengle/msan622/template/homework0/anscombe.r")
+source_url("https://github.com/Cindy597/msan622/blob/master/homework1/cindy_homework1.r")
 ```
 
-This will generate 1 image and some text output. See below for details.
+This will generate 4 images, including a scatterplot, bar chat, small multiples plot and nulti-line plot. See the following Discussion part for details.
+
+Also, in order to keep the generated four png files, please set up you own working directory before running the code by entering the following command in R:
+
+```
+setwd("/working directory") 
+```
+
+
 
 ## Discussion ##
 
-[DISCUSSION: Include a discussion here if required by the assignment.]
+First, I performed some transformations of movies dataset and EuStockMarkets dataset in R to prepare for the visualization.
+Second, I created 4 plots by using R and ggplot2 according to the requirements.
 
-First, notice that many of the simple statistics for these four series are the same:
+Plot1:  Produced a scatterplot from the movies dataset in the ggplot2 package, where budget is shown on the x-axis and rating is shown on the y-axis.
 
-```R
-> print(stats)
-  group mean_x   mean_y var_x    var_y correlation lm_intercept lm_x_effect
-1     1      9 7.500909    11 4.127269   0.8164205     3.000091   0.5000909
-2     2      9 7.500909    11 4.127629   0.8162365     3.000909   0.5000000
-3     3      9 7.500000    11 4.122620   0.8162867     3.002455   0.4997273
-4     4      9 7.500909    11 4.123249   0.8165214     3.001727   0.4999091
-```
+![IMAGE](hw1-scatter.png)
 
-However, when we plot these four series as scatterplots with trend lines, we can see they each have a distinct pattern:
+First of all, since 'budget' displays in E-notation, which is not simple for audience to understand quickly, I scaled the budget from dollar to million unit; 
+I changed the point size by `size=2.5` in `geom_point` to avoid messy plot because there are too many data points. In addition, instead of easily plotting 
+'budget' and 'rating',I grouped the data points based on 'genre' by adding `aes(colour=factor(genre)` in `geom_point`.In order to make the plot beautiful, I changed
+the legend title, and I also changed the size and color of all text elements in the plot, including plot title, xlab, ylab,legend title, and legend text to help 
+audience understand the insights of this plot clearly. 
 
-![IMAGE](anscombe_quartet.png)
 
-While this is a simple (and contrived) example, it does help motivate how visualization can help us quickly see patterns in data.
+Plot2: Counted the number of action, adventure, etc. movies in the genre column of the movies dataset, and produced a bar plot.
+
+![IMAGE](hw1-bar.png)
+
+In order to lead audience only focus on the count of movies by genre, instead of color or shape of the plot, I just used single and grey color by `fill="#56B4E9"` in `geom_bar`.
+For audience's convenience to compare the count of each genre type, I plotted a decreasing bar plot by creating a new movie dataset based on decreasing 'genre' levels, using the R 
+code shown as below:
+`my_movies<- within(movies, genre <- factor(genre, levels=names(sort(table(genre), decreasing=TRUE))))`.
+In addition to change the size and color of all text elements in the plot as plot1, I also changed the color of axis.text to red, to highlight the movie type and count numbers.
+
+
+Plot3: Produced a multi-line chart from the eu dataset with time shown on the x-axis and price on the y-axis
+
+![IMAGE](hw1-multiples.png)
+
+Plot4:
+
+![IMAGE](hw1-multiline.png)
--- homework1/cindy_homework1.r
+++ homework1/cindy_homework1.r
@@ -1,9 +1,15 @@
+__author__='Can(Cindy) Jin'
+
+#please fill in your working directory to keep the generated plots 
+setwd("/Users/cindy/Desktop")
 # load the datasets
 library(ggplot2) 
 data(movies) 
 data(EuStockMarkets)
+
 #Filter out any rows that have a budget value less than or equal to 0 in the movies dataset.
 movies<-subset(movies, movies$budge>0)
+
 #Add a genre column to the movies dataset
 genre <- rep(NA, nrow(movies))
 count <- rowSums(movies[, 18:24])
@@ -17,50 +23,74 @@ genre[which(count == 1 & movies$Documentary == 1)] = "Documentary"
 genre[which(count == 1 & movies$Romance == 1)] = "Romance"
 genre[which(count == 1 & movies$Short == 1)] = "Short"
 movies$genre<-genre
-movies$count<-count
+
 #Transform the EuStockMarkets dataset to a time series
 eu <- transform(data.frame(EuStockMarkets), time = time(EuStockMarkets))
 
+#Plot1:
 #Produce a scatterplot from the movies dataset in the ggplot2 package, 
 #where budget is shown on the x-axis and rating is shown on the y-axis
+movies$budget<-movies$budget/1000000
 p1<-ggplot(movies,aes(x=budget,y=rating))+
-  geom_point(colour='red',size=3)+
-  ggtitle("budget v/s rating")+
-  xlab("budget")+
-  ylab("rating")
+  geom_point(aes(colour=factor(genre)),size=2.5)+
+  labs(colour='Genre')+
+  ggtitle("Movie Rating versus Budget")+
+  xlab("Budget (Million)")+
+  ylab("Rating")+
+  theme(text = element_text(size = 15, colour = "blue"))
 print(p1)
+ggsave("hw1-scatter.png",p1,width=10,height=6, dpi=500)
 
+
+
+#Plot2
 #Count the number of action, adventure, etc. movies in the genre column of 
 #the movies dataset. Show the results as a bar chart
-p2<-ggplot(movies,aes(x=factor(genre),color=factor(genre),fill=factor(genre)))+
-  geom_bar()+
-  ggtitle("count of different movie types")+
-  xlab("movie_type")+
-  ylab("count")
+#created a new movie dataset based on decreasing 'genre' levels, in order to plot decreasing bar chat
+my_movies<- within(movies, genre <- factor(genre, levels=names(sort(table(genre), decreasing=TRUE))))
+p2<-ggplot(my_movies,aes(x=genre))+
+  geom_bar(binwidth=1,fill="#56B4E9")+
+  ggtitle("Count of Movies on Genre")+
+  xlab("Genre")+
+  ylab("Count")+
+  theme(text = element_text(size = 16, colour = "black"),axis.text = element_text(colour = "red"))
 print(p2)
+ggsave("hw1-bar.png",p2,width=10,height=6, dpi=500)
 
+
+
+#Plot3
 #Use the genre column in the movies dataset to generate a small-multiples scatterplot using the facet_wrap() function such that 
 #budget is shown on the x-axis and rating is shown on the y-axis
 p3<-ggplot(movies)+
-  geom_point(aes(x=budget,y=rating,colour=genre),size=1.5)+
-  ggtitle("budget v/s rating for different movie type")+
-  xlab("budget")+
-  ylab("rating")+
-  facet_wrap(~genre)
+  geom_point(aes(x=budget,y=rating,colour=genre),size=2.0)+
+  ggtitle("Budget versus Rating by Genre")+
+  xlab("Budget (Million)")+
+  ylab("Rating")+
+  facet_wrap(~genre,nrow=3)+
+  theme(text = element_text(size = 15, colour = "black"),axis.text = element_text(colour = "red"),legend.text=element_text(size=9))
 print(p3)
+ggsave("hw1-multiples.png",p3,width=10,height=6, dpi=500)
 
+
+
+#Plot4
 #Produce a multi-line chart from the eu dataset with time shown on the x-axis 
 #and price on the y-axis
-
-type<-as.data.frame(c(rep(colnames(eu)[1],length(eu$DAX)),rep(colnames(eu)[2],length(eu$SMI)),rep(colnames(eu)[3],length(eu$CAC)),rep(colnames(eu)[4],length(eu$FTSE))))
+index<-as.data.frame(c(rep(colnames(eu)[1],length(eu$DAX)),rep(colnames(eu)[2],length(eu$SMI)),rep(colnames(eu)[3],length(eu$CAC)),rep(colnames(eu)[4],length(eu$FTSE))))
 price<-as.data.frame(c(eu$DAX,eu$SMI,eu$CAC,eu$FTSE))
 time<-as.data.frame(rep(eu$time,4))
-mydata<-cbind(type,price,time)
-colnames(mydata)<-c("type","price","time")
+mydata<-cbind(index,price,time)
+colnames(mydata)<-c("index","price","time")
 
 p4<- ggplot(mydata)+
-  geom_line(aes(x=time,y=price, group=factor(type), color=factor(type)))+
-  ggtitle("price for different stock")+
+  geom_line(aes(x=time,y=price, group=factor(index), color=factor(index)),size=0.8)+
+  ggtitle("Stock Price Trend for 4 different Indexes ")+
+  labs(colour='Index')+
   xlab("time")+
-  ylab("price")
+  ylab("price")+
+  theme(text = element_text(size = 15, colour = "blue"), legend.text=element_text(size=17,colour="red"))
 print(p4)
+ggsave("hw1-multiline.png",p4,width=10,height=6, dpi=500)
+
+
